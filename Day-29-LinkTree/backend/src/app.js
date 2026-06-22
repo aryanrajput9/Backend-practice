5f3752e43c5405944483ca3a26e9c8f1de9f36f2
aryanrajput9 <babusahebaryan4@gmail.com>
2026-06-22 23:32:44 +0530

    update corse url

--- Day-29-LinkTree/backend/src/app.js
+++ Day-29-LinkTree/backend/src/app.js
@@ -12,7 +12,7 @@ function createApp() {
 
     app.use(
         cors({
-            origin: "https://backend-practice-aed9td2n8-aryanrajput9s-projects.vercel.app",
+            origin: "https://backend-practice-lhab3yziw-aryanrajput9s-projects.vercel.app/",
             credentials: true,
         })
     );
