be5d0aea13329e2efc2bb5f62b4c365eeb3ec267
aryanrajput9 <babusahebaryan4@gmail.com>
2026-06-22 22:41:12 +0530

    update

--- Day-29-LinkTree/frontend/src/features/home/services/home.services.js
+++ Day-29-LinkTree/frontend/src/features/home/services/home.services.js
@@ -1,6 +1,6 @@
-import axios from 'axios';
+import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: "/api/",
-    withCredentials: true
+    baseURL: "https://backend-lr35.onrender.com/api",
+    withCredentials: true,
 });
